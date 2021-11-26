58bd7e8b90619b43e548738b679f7b43bce4ca27
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-11-27 06:29:24 +0900

    update setup.sh

--- docs/setup.sh
+++ docs/setup.sh
@@ -6,16 +6,19 @@ mkdir -p /tmp/docs
 wget -P /tmp/docs https://bcl-group.github.io/ubuntu-setup-script/ubuntu-setup-script.sh.enc
 
 # Install Ruby
-sudo http_proxy=$http_proxy apt update
-sudo http_proxy=$http_proxy apt install git gcc make ruby-dev libssl-dev zlib1g-dev whiptail -y
-git clone --depth 1 https://github.com/rbenv/rbenv ~/.rbenv
-mkdir -p ~/.rbenv/plugins
-git clone https://github.com/rbenv/ruby-build ~/.rbenv/plugins/ruby-build
-echo -e "\nexport PATH=\"$PATH:$HOME/.rbenv/bin\"\n" >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
-rbenv install 3.0.2
-rbenv global 3.0.2
+
+if not test $(which ruby); then
+    sudo http_proxy=$http_proxy apt update
+    sudo http_proxy=$http_proxy apt install git gcc make ruby-dev libssl-dev zlib1g-dev whiptail -y
+    git clone --depth 1 https://github.com/rbenv/rbenv ~/.rbenv
+    mkdir -p ~/.rbenv/plugins
+    git clone https://github.com/rbenv/ruby-build ~/.rbenv/plugins/ruby-build
+    echo -e "\nexport PATH=\"$PATH:$HOME/.rbenv/bin\"\n" >> ~/.bashrc
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    source ~/.bashrc
+    rbenv install 3.0.2
+    rbenv global 3.0.2
+fi
 
 gem install specific_install
 gem specific_install bcl-group/ubuntu-setup-script dev
