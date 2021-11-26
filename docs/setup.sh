37eae1748697fd9d91d64565b0a7fae13640ca4f
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-11-27 06:16:23 +0900

    update setup.sh

--- docs/setup.sh
+++ docs/setup.sh
@@ -2,7 +2,8 @@
 
 export http_proxy=http://proxy.cc.yamaguchi-u.ac.jp:8080
 export https_proxy=https://proxy.cc.yamaguchi-u.ac.jp:8080
-wget -P /tmp https://bcl-group.github.io/ubuntu-setup-script/ubuntu-setup-script.sh.enc
+mkdir -p /tmp/docs
+wget -P /tmp/docs https://bcl-group.github.io/ubuntu-setup-script/ubuntu-setup-script.sh.enc
 
 # Install Ruby
 sudo http_proxy=$http_proxy apt update
@@ -17,8 +18,9 @@ rbenv install 3.0.2
 rbenv global 3.0.2
 
 gem install specific_install
-gem install bcl-group/ubuntu-setup-script dev
+gem specific_install bcl-group/ubuntu-setup-script dev
 
+cd /tmp && bcl-install-decode || exit 1
 
-# chmod u+x /tmp/dec.sh
-# /tmp/dec.sh && chmod u+x /tmp/ubuntu-setup-script.sh && /tmp/ubuntu-setup-script.sh
+chmod u+x /tmp/ubuntu-setup-script.sh
+/tmp/ubuntu-setup-script.sh
