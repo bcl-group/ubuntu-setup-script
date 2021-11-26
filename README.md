cdd8735b39cf5da32099ab55d778420527c087f1
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-11-27 05:59:40 +0900

    update setup.sh

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ubuntu-setup-script
-詳しくは: https://github.com/bcl-group/memo/wiki/Configuration-for-Ubuntu-20.04,-20.10
+研究室の Ubuntu PC セットアップ用スクリプト
+スクリプトはセキュリティー上の理由から暗号化されています。
+
+## スクリプトの編集
+スクリプトを変更するには、暗号化されたスクリプトをデコードする必要があります。
+
+```bash
+cd <リポジトリー>
+bcl-install-decode
+# ./docs/ubuntu-setup-script.sh.enc が復号されて、 ./ubuntu-setup-script.sh が作成
+```
+
+## スクリプトを暗号化
+```bash
+cd <リポジトリー>
+bcl-install-encode
+```
--- docs/setup.sh
+++ docs/setup.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-wget -P /tmp https://bcl-group.github.io/ubuntu-setup-script/dec.sh
+export http_proxy=http://proxy.cc.yamaguchi-u.ac.jp:8080
+export https_proxy=https://proxy.cc.yamaguchi-u.ac.jp:8080
 wget -P /tmp https://bcl-group.github.io/ubuntu-setup-script/ubuntu-setup-script.sh.enc
-chmod u+x /tmp/dec.sh
-/tmp/dec.sh && chmod u+x /tmp/ubuntu-setup-script.sh && /tmp/ubuntu-setup-script.sh
+
+# Install Ruby
+sudo http_proxy=$http_proxy apt update
+sudo http_proxy=$http_proxy apt install git gcc make ruby-dev libssl-dev zlib1g-dev whiptail -y
+git clone --depth 1 https://github.com/rbenv/rbenv ~/.rbenv
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build ~/.rbenv/plugins/ruby-build
+echo -e "\nexport PATH=\"$PATH:$HOME/.rbenv/bin\"\n" >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+rbenv install 3.0.2
+rbenv global 3.0.2
+
+gem install specific_install
+gem install bcl-group/ubuntu-setup-script dev
+
+
+# chmod u+x /tmp/dec.sh
+# /tmp/dec.sh && chmod u+x /tmp/ubuntu-setup-script.sh && /tmp/ubuntu-setup-script.sh
