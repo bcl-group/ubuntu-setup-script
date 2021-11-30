e7eb0e848e5649183967bf59882b781ce77a68b0
hikari <39254183+Himeyama@users.noreply.github.com>
2021-11-30 12:54:32 +0900

    Update setup.sh

--- docs/setup.sh
+++ docs/setup.sh
@@ -8,6 +8,7 @@ wget -P /tmp/docs https://bcl-group.github.io/ubuntu-setup-script/ubuntu-setup-s
 
 # Install Ruby
 
+export RUBY_BUILD_CURL_OPTS="-k -x http://proxy.cc.yamaguchi-u.ac.jp:8080"
 if [ ! -d $HOME/.rbenv ]; then
     sudo http_proxy=$http_proxy apt update
     sudo http_proxy=$http_proxy apt install git gcc make ruby-dev libssl-dev zlib1g-dev whiptail -y
@@ -18,8 +19,7 @@ if [ ! -d $HOME/.rbenv ]; then
     echo -e "\nexport PATH=\"$PATH:$HOME/.rbenv/bin\"\n" >> ~/.bashrc
     echo 'eval "$(rbenv init -)"' >> ~/.bashrc
     source ~/.bashrc
-    export RUBY_BUILD_CURL_OPTS="-k -x http://proxy.cc.yamaguchi-u.ac.jp:8080"
-    rbenv install 3.0.3
+    rbenv install 3.0.3 -s
     rbenv global 3.0.3
 else
     rbenv install 3.0.3 -s
