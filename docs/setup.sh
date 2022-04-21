df709e9b045f2356592ba4be95d2d9136e6af57d
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2022-04-21 12:38:32 +0900

    update setup.sh

--- docs/setup.sh
+++ docs/setup.sh
@@ -13,7 +13,8 @@ wget -P /tmp/docs https://bcl-group.github.io/ubuntu-setup-script/ubuntu-setup-s
 export RUBY_BUILD_CURL_OPTS="-k -x http://proxy.cc.yamaguchi-u.ac.jp:8080"
 if [ ! -d $HOME/.rbenv ]; then
     sudo http_proxy=$http_proxy apt update
-    sudo http_proxy=$http_proxy apt install git gcc make ruby-dev libssl-dev zlib1g-dev whiptail -y
+    sudo http_proxy=$http_proxy apt install git gcc make ruby-dev libssl-dev zlib1g-dev whiptail \
+        autoconf bison build-essential libyaml-dev libreadline6-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev -y
     git clone --depth 1 https://github.com/rbenv/rbenv ~/.rbenv
     mkdir -p ~/.rbenv/plugins
     # git clone https://github.com/rbenv/ruby-build ~/.rbenv/plugins/ruby-build
