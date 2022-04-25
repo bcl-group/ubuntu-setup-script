d02d9f00b270a60892f83036abcbcfbb26abccab
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2022-04-25 11:10:54 +0900

    modify ldap

--- docs/setup.sh
+++ docs/setup.sh
@@ -15,17 +15,6 @@ if [ ! -d $HOME/.rbenv ]; then
     sudo http_proxy=$http_proxy apt update
     sudo http_proxy=$http_proxy apt install git gcc make ruby-dev libssl-dev zlib1g-dev whiptail \
         autoconf bison build-essential libyaml-dev libreadline6-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev -y
-    
-    # rbenv のインストール
-    wget -q https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer -O- | bash
-
-    is_rbenv_path=$(cat $HOME/.bashrc | grep PATH | grep rbenv)
-    if [[ -z "$is_rbenv_path" ]]; then
-        export PATH=$PATH:$HOME/.rbenv/bin
-        echo -e "\nexport PATH=\"$PATH:$HOME/.rbenv/bin\"\n" >> ~/.bashrc
-        echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-        source ~/.bashrc
-    fi   
 fi
 
 export GEM_HOME=$HOME/.gem
